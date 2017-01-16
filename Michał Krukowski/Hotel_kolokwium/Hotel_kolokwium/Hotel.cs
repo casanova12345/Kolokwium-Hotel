021cab9103bd7fa97ae54c1ae701b9a714fe184e
casanova12345 <krukowskim123@gmail.com>
2017-01-16 18:48:03 +0100

    Add files via upload

---
+++ Michał Krukowski/Hotel_kolokwium/Hotel_kolokwium/Hotel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotel_kolokwium
+{
+    class Hotel : IHotel, IData
+    {
+        private SortedList<Pokoj, Gosc> Rezerwacje = new SortedList<Pokoj, Gosc>();
+        public SortedList<Pokoj, Gosc> rezerwacje { get { return Rezerwacje; } }
+        private double zysk = -80;
+        private DateTime Data;
+
+        public void DodajRezerwacje(string imie, string nazwisko, int nr, double cena)
+        {
+            Gosc osoba = new Gosc(imie, nazwisko);
+            Pokoj innypokoj = new Pokoj(nr, cena);
+            zysk += cena;
+            Rezerwacje.Add(innypokoj, osoba);
+          
+        }
+        public void  OdwolajRezerwacje()
+        {
+          
+        }
+        public void UstawDate(DateTime data)
+        {
+            this.Data = data;
+        }
+        public bool SprawdzDate()
+        {
+            return (Data > DateTime.Now) ? true : false;
+        }
+        public override string ToString()
+        {
+            string wyjscie = "Rezerwacje:\n\nData: " + Data.ToShortDateString();
+            foreach (var a in Rezerwacje)
+            {
+                wyjscie += "\n\n";
+                string Pokoj = a.Key.ToString() + "; " + a.Value.ToString();
+                wyjscie += "[" + Pokoj + "]";
+            }
+            wyjscie += "\n\nZysk: " + zysk;
+            return wyjscie;
+        }
+    }
+}
---
+++ Michał Krukowski/Hotel_kolokwium/Hotel_kolokwium/Pokoj.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotel_kolokwium
+{
+    class Pokoj : IComparable
+    {
+        private int nrPokoju;
+        private double cenaZaDzien;
+
+        public Pokoj(int nrPokoju, double cenaZaDzien)
+        {
+            this.nrPokoju = nrPokoju;
+            this.cenaZaDzien = cenaZaDzien;
+        }
+
+        public override string ToString()
+        {
+            return "Pokoj, nr " + nrPokoju +"cena za dzien: " + cenaZaDzien;
+        }
+
+        public double zwrocNrPokoju()
+        {
+            return nrPokoju;
+        }
+        public double zwrocCeneZaDzien()
+        {
+            return cenaZaDzien;
+        }
+
+        public int CompareTo(object obj)
+        {
+            if (obj == null) return 1;
+            Pokoj innyPokoj = obj as Pokoj;
+            if (innyPokoj != null)
+                return this.nrPokoju.CompareTo(innyPokoj.nrPokoju);
+            else
+                throw new ArgumentException("Objekt nie jest nr pokoju");
+        }
+    }
+}
